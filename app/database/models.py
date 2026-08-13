96997cb61c16a88bb158b21d735eefc0adabe3cd
danielgelchu0-sketch <danielgelchu0@gmail.com>
2026-08-13 22:47:16 +0300

    change the models.py and adde another import files

--- app/database/models.py
+++ app/database/models.py
@@ -6,8 +6,12 @@ from sqlalchemy import (
     Numeric,
     String,
 )
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+)
 
 class Base(DeclarativeBase):
     pass
